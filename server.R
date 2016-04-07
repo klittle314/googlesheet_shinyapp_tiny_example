536b9acfddecc36c587b780e1d245cc9ad9e0b9c
klittle314 <klittle@iecodesign.com>
2016-04-06 21:00:01 -0500

    two versions of server code
    One version uses the reactive trick to upload the authorization file;
    one version is "old school", which will fail because there is no
    .httr-oauth file on the shinyapps.io server

--- server.R
+++ server.R
@@ -5,26 +5,19 @@
 #source("global.R")
 library(shiny)
 library(googlesheets)
-# paste(getwd())
 
-#options("googlesheets.httr_oauth_cache" = "gs_auth")
-# gskey2 <- "1QhPtrsukBCVZJ-t-nSxCLR1J2OCX-yVReAGVYXIzexM"
-# gs_file <- gs_key(x=gskey2)
-# gs_df <- gs_read(gs_file)
-
+#code that uses gs_auth as authorization file
 shinyServer(function(input, output, session) {
   gs_df <- reactive({
     #Example key for my simple spreadsheet on google drive
     options("googlesheets.httr_oauth_cache" = "gs_auth")
     gskey2 <- "1QhPtrsukBCVZJ-t-nSxCLR1J2OCX-yVReAGVYXIzexM"
-
     gs_auth()
     # register the googlesheet
     gs_file <- gs_key(x=gskey2)
     gs_df <- gs_read(gs_file)
   })
 
-  #df = gs_df
 
   # print table using input from Google Sheet
   output$table1 = renderDataTable(gs_df())
@@ -37,7 +30,6 @@ shinyServer(function(input, output, session) {
         paste('data-', Sys.Date(), '.csv', sep='')
       },
       content = function(con) {
-         #write.csv(df,con)
         
         write.csv(gs_df(), con)
       }
@@ -46,3 +38,25 @@ shinyServer(function(input, output, session) {
 )
 
 
+#uncomment the following lines to experience the error
+# gskey2 <- "1QhPtrsukBCVZJ-t-nSxCLR1J2OCX-yVReAGVYXIzexM"
+# gs_file <- gs_key(x=gskey2)
+# gs_df <- gs_read(gs_file)
+#shinyServer(function(input, output, session) {
+#df = gs_df
+
+
+# output$table1 = renderDataTable(df)
+# 
+# 
+# output$downloadData <- downloadHandler(
+#   filename = function() {
+#     paste('data-', Sys.Date(), '.csv', sep='')
+#   },
+#   content = function(con) {
+#     write.csv(df,con)
+# 
+#   }
+# )
+# }
+#)
