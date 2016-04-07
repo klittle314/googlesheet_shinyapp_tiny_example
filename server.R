6cfe641397b1152fa6fc7511e4553e90ab7c936d
klittle314 <klittle@iecodesign.com>
2016-04-07 10:55:07 -0500

    edits to comments
    to match README.md comments

--- server.R
+++ server.R
@@ -7,56 +7,56 @@ library(shiny)
 library(googlesheets)
 
 #code that uses gs_auth as authorization file
-shinyServer(function(input, output, session) {
-  gs_df <- reactive({
-    #Example key for my simple spreadsheet on google drive
-    options("googlesheets.httr_oauth_cache" = "gs_auth")
-    gskey2 <- "1QhPtrsukBCVZJ-t-nSxCLR1J2OCX-yVReAGVYXIzexM"
-    gs_auth()
-    # register the googlesheet
-    gs_file <- gs_key(x=gskey2)
-    gs_df <- gs_read(gs_file)
-  })
-
-
-  # print table using input from Google Sheet
-  output$table1 = renderDataTable(gs_df())
-  # 
-  #output$table1 = renderDataTable(df)
-  
-  # download file
-  output$downloadData <- downloadHandler(
-      filename = function() {
-        paste('data-', Sys.Date(), '.csv', sep='')
-      },
-      content = function(con) {
-        
-        write.csv(gs_df(), con)
-      }
-    )
- }
-)
-
-
-#uncomment the following lines to experience the error
-# gskey2 <- "1QhPtrsukBCVZJ-t-nSxCLR1J2OCX-yVReAGVYXIzexM"
-# gs_file <- gs_key(x=gskey2)
-# gs_df <- gs_read(gs_file)
-#shinyServer(function(input, output, session) {
-#df = gs_df
-
-
-# output$table1 = renderDataTable(df)
+# shinyServer(function(input, output, session) {
+#   gs_df <- reactive({
+#     #Example key for my simple spreadsheet on google drive
+#     options("googlesheets.httr_oauth_cache" = "gs_auth")
+#     gskey2 <- "1QhPtrsukBCVZJ-t-nSxCLR1J2OCX-yVReAGVYXIzexM"
+#     gs_auth()
+#     # register the googlesheet
+#     gs_file <- gs_key(x=gskey2)
+#     gs_df <- gs_read(gs_file)
+#   })
 # 
 # 
-# output$downloadData <- downloadHandler(
-#   filename = function() {
-#     paste('data-', Sys.Date(), '.csv', sep='')
-#   },
-#   content = function(con) {
-#     write.csv(df,con)
-# 
-#   }
+#   # print table using input from Google Sheet
+#   output$table1 = renderDataTable(gs_df())
+#   # 
+#   #output$table1 = renderDataTable(df)
+#   
+#   # download file
+#   output$downloadData <- downloadHandler(
+#       filename = function() {
+#         paste('data-', Sys.Date(), '.csv', sep='')
+#       },
+#       content = function(con) {
+#         
+#         write.csv(gs_df(), con)
+#       }
+#     )
+#  }
 # )
-# }
-#)
+
+
+#uncomment the following lines to experience the error:  you cannot upload the .httr-oauth file?
+gskey2 <- "1QhPtrsukBCVZJ-t-nSxCLR1J2OCX-yVReAGVYXIzexM"
+gs_file <- gs_key(x=gskey2)
+gs_df <- gs_read(gs_file)
+shinyServer(function(input, output, session) {
+df = gs_df
+
+
+output$table1 = renderDataTable(df)
+#
+#
+output$downloadData <- downloadHandler(
+  filename = function() {
+    paste('data-', Sys.Date(), '.csv', sep='')
+  },
+  content = function(con) {
+    write.csv(df,con)
+
+  }
+)
+}
+)
